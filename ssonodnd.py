f0e1974dcd1e0d50f2fc6ada197d1e91c6f613f7
ssono <blusentinel32@gmail.com>
2017-10-03 14:01:55 -0400

    init

---
+++ ssonodnd.py
@@ -0,0 +1,25 @@
+info = {
+    "name": 0,
+    "level": 2,
+    "class": "ranger",
+    "paragon": "",
+    "epic": "",
+    "xp": 0,
+    "race": "",
+    "size": "",
+    "age": "",
+    "gender": "",
+    "height": "",
+    "weight": "",
+    "alignment": "",
+    "deity"
+}
+
+ability = {
+    "str": 0,
+    "con": 0,
+    "dex": 0,
+    "int": 0,
+    "wis": 0,
+    "cha": 0
+}
