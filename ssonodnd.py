ee06a83d3b8b23df7b0ea66ea31908fa14b13eb6
ssono <blusentinel32@gmail.com>
2017-10-04 16:43:55 -0400

    skills

--- ssonodnd.py
+++ ssonodnd.py
@@ -1,25 +1,46 @@
 info = {
-    "name": 0,
+    "name": "Kraven Moarhed",
     "level": 2,
     "class": "ranger",
-    "paragon": "",
-    "epic": "",
-    "xp": 0,
-    "race": "",
-    "size": "",
-    "age": "",
-    "gender": "",
-    "height": "",
-    "weight": "",
-    "alignment": "",
-    "deity"
+    "paragon": "none",
+    "epic": "none",
+    "xp": 1025,
+    "race": "halfling",
+    "size": "small",
+    "age": "24",
+    "gender": "male",
+    "height": "4'0",
+    "weight": "80 lb",
+    "alignment": "chaotic neutral",
+    "deity": "Loki"
 }
 
 ability = {
-    "str": 0,
-    "con": 0,
-    "dex": 0,
-    "int": 0,
-    "wis": 0,
-    "cha": 0
+    "str": 14,
+    "con": 10,
+    "dex": 22,
+    "int": 12,
+    "wis": 14,
+    "cha": 13
+}
+
+#(skill: trained, misc bonus)
+skills = {
+    "acrobatics": (True, 2),
+    "arcana": (False, 0),
+    "athletics": (True, 0),
+    "bluff": (False, 0),
+    "diplomacy": (False, 0),
+    "dungeoneering": (True, 0),
+    "endurance": (False, 0),
+    "heal": (False, 0),
+    "history": (False, 0),
+    "insight": (False, 0),
+    "intimidate": (False, 0),
+    "nature": (True, 0),
+    "perception": (True, 0),
+    "religion": (False, 0),
+    "stealth": (True, 0),
+    "streetwise": (False, 0),
+    "thievery": (True, 2)
 }
