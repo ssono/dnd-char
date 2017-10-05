124b2bbe6393b5b41e3e096ede780e4895fd93fc
ssono <blusentinel32@gmail.com>
2017-10-04 23:24:31 -0400

    added check comand

--- ssonodnd.py
+++ ssonodnd.py
@@ -1,7 +1,8 @@
 import json
 import random
+import sys
 
-"""
+
 #basic character info
 info = {
     "name": "Kraven Moarhed",
@@ -32,23 +33,23 @@ ability = {
 
 #(skill: trained, misc bonus]
 skills = {
-    "acrobatics": [True, 2],
-    "arcana": [False, 0],
-    "athletics": [True, 0],
-    "bluff": [False, 0],
-    "diplomacy": [False, 0],
-    "dungeoneering": [True, 0],
-    "endurance": [False, 0],
-    "heal": [False, 0],
-    "history": [False, 0],
-    "insight": [False, 0],
-    "intimidate": [False, 0],
-    "nature": [True, 0],
-    "perception": [True, 0],
-    "religion": [False, 0],
-    "stealth": [True, 0],
-    "streetwise": [False, 0],
-    "thievery": [True, 2]
+    "acr": [5, 2],
+    "arc": [0, 0],
+    "ath": [5, 0],
+    "blu": [0, 0],
+    "dip": [0, 0],
+    "dun": [5, 0],
+    "end": [0, 0],
+    "hea": [0, 0],
+    "his": [0, 0],
+    "ins": [0, 0],
+    "int": [0, 0],
+    "nat": [5, 0],
+    "per": [5, 0],
+    "rel": [0, 0],
+    "ste": [5, 0],
+    "str": [0, 0],
+    "thi": [5, 2]
 }
 
 
@@ -128,7 +129,7 @@ with open(skiLoad, 'w') as f_obj:
 bagLoad = 'bag.json'
 with open(bagLoad, 'w') as f_obj:
     json.dump(bag, f_obj)
-"""
+
 
 ###############################################################################################################################
 
@@ -148,6 +149,7 @@ bagDump = 'bag.json'
 with open(bagDump) as f_obj:
     bag = json.load(f_obj)
 
+##############################################################################################################################################
 #returns value of a roll. dice = [dice required for roll], bonuses[bonuses for specific roll]
 
 def roll(dice, bonuses):
@@ -158,17 +160,16 @@ def roll(dice, bonuses):
         result += b
     return result
 
+def d20():
+    return random.randrange(1, 20)
+#####################################################################################################################################################
 initiative = (info["level"]/2) + (ability["dex"] - 10)/2
 
 speed = 6
 
-passiveInsight = 10 + skills["insight"][1] + (info["level"]/2) + (ability["wis"] - 10)/2
-if skills["insight"][0]:
-    passiveInsight += 5
+passiveInsight = 10 + skills["ins"][1] + (info["level"]/2) + (ability["wis"] - 10)/2 + skills["ins"][0]
 
-passivePerception = 10 + skills["perception"][1] + info["level"]/2 + (ability["wis"] - 10)/2
-if skills["perception"][0]:
-    passivePerception += 5
+passivePerception = 10 + skills["per"][1] + info["level"]/2 + (ability["wis"] - 10)/2 + skills["per"][0]
 
 maxHP = 12 + ability["con"] + 5*(info["level"] - 1)
 HP = maxHP
@@ -184,7 +185,7 @@ WILL = 10 + info["level"]/2 + (ability["wis"] - 10)/2
 weight = 0
 for item in bag:
     weight += bag[item]["quantity"] * bag[item]["weight"]
-
+#################################################################################################################################################
 def status():
     print(
         "HP:\t" + str(HP) +"\n" +
@@ -198,19 +199,75 @@ def status():
         "passive insight:\t" + str(passiveInsight) +"\n" +
         "passive perception:\t" + str(passivePerception) +"\n"
     )
+#########################################################################################################################################################################
 def check():
-    skill = input("""What would you like to check?\n (acr)obatics, (arc)ana, (ath)letics, (blu)ff, (dip)lomacy, (dun)geoneering, (end)urance, (hea)l,
-                    (his)tory, (ins)ight, (int)imidate, (nat)ure, (per)ception, (rel)igion, (ste)alth, (str)eetwise, (thi)every\n\n""")
-
-
-#loop that asks what you want to do. (s)tatus, (c)heck, (a)ction, (p)ower, (b)ag, (i)nfo, (e)dit
+    skill = input("""What would you like to check?\n (acr)obatics, (arc)ana, (ath)letics, (blu)ff, (dip)lomacy, (dun)geoneering,\n (end)urance, (hea)l, (his)tory, (ins)ight, (int)imidate, (nat)ure, (per)ception (rel)igion, (ste)alth, (str)eetwise, (thi)every\n\n""")
+    print("\n")
+    skill = skill.lower()
+    result = "That is not a skill you possess..."
+    if skill == "acr":
+        roll = d20()
+        return("acr:\t" + str(roll + info["level"]/2 + (ability["dex"]-10)/2 + skills["acr"][0] + skills["acr"][1]) + "\troll:\t" + str(roll))
+    elif skill == "arc":
+        roll = d20()
+        return("arc:\t" + str(roll + info["level"]/2 + (ability["int"]-10)/2 + skills["arc"][0] + skills["arc"][1]) + "\troll:\t" + str(roll))
+    elif skill == "ath":
+        roll = d20()
+        return("ath:\t" + str(roll + info["level"]/2 + (ability["str"]-10)/2 + skills["ath"][0] + skills["ath"][1]) + "\troll:\t" + str(roll))
+    elif skill == "blu":
+        roll = d20()
+        return("blu:\t" + str(roll + info["level"]/2 + (ability["cha"]-10)/2 + skills["blu"][0] + skills["blu"][1]) + "\troll:\t" + str(roll))
+    elif skill == "dip":
+        roll = d20()
+        return("dip:\t" + str(roll + info["level"]/2 + (ability["cha"]-10)/2 + skills["dip"][0] + skills["dip"][1]) + "\troll:\t" + str(roll))
+    elif skill == "dun":
+        roll = d20()
+        return("dun:\t" + str(roll + info["level"]/2 + (ability["wis"]-10)/2 + skills["dun"][0] + skills["dun"][1]) + "\troll:\t" + str(roll))
+    elif skill == "end":
+        roll = d20()
+        return("end:\t" + str(roll + info["level"]/2 + (ability["con"]-10)/2 + skills["end"][0] + skills["end"][1]) + "\troll:\t" + str(roll))
+    elif skill == "hea":
+        roll = d20()
+        return("hea:\t" + str(roll + info["level"]/2 + (ability["wis"]-10)/2 + skills["hea"][0] + skills["hea"][1]) + "\troll:\t" + str(roll))
+    elif skill == "his":
+        roll = d20()
+        return("his:\t" + str(roll + info["level"]/2 + (ability["int"]-10)/2 + skills["his"][0] + skills["his"][1]) + "\troll:\t" + str(roll))
+    elif skill == "ins":
+        roll = d20()
+        return("ins:\t" + str(roll + info["level"]/2 + (ability["wis"]-10)/2 + skills["ins"][0] + skills["ins"][1]) + "\troll:\t" + str(roll))
+    elif skill == "int":
+        roll = d20()
+        return("int:\t" + str(roll + info["level"]/2 + (ability["cha"]-10)/2 + skills["int"][0] + skills["int"][1]) + "\troll:\t" + str(roll))
+    elif skill == "nat":
+        roll = d20()
+        return("nat:\t" + str(roll + info["level"]/2 + (ability["wis"]-10)/2 + skills["nat"][0] + skills["nat"][1]) + "\troll:\t" + str(roll))
+    elif skill == "per":
+        roll = d20()
+        return("per:\t" + str(roll + info["level"]/2 + (ability["wis"]-10)/2 + skills["per"][0] + skills["per"][1]) + "\troll:\t" + str(roll))
+    elif skill == "rel":
+        roll = d20()
+        return("rel:\t" + str(roll + info["level"]/2 + (ability["int"]-10)/2 + skills["rel"][0] + skills["rel"][1]) + "\troll:\t" + str(roll))
+    elif skill == "ste":
+        roll = d20()
+        return("ste:\t" + str(roll + info["level"]/2 + (ability["dex"]-10)/2 + skills["ste"][0] + skills["ste"][1]) + "\troll:\t" + str(roll))
+    elif skill == "str":
+        roll = d20()
+        return("str:\t" + str(roll + info["level"]/2 + (ability["cha"]-10)/2 + skills["str"][0] + skills["str"][1]) + "\troll:\t" + str(roll))
+    elif skill == "thi":
+        roll = d20()
+        return("thi:\t" + str(roll + info["level"]/2 + (ability["dex"]-10)/2 + skills["thi"][0] + skills["thi"][1]) + "\troll:\t" + str(roll))
+    return result
+#################################################################################################################################################################################################3
+#loop that asks what you want to do. (s)tatus, (c)heck, (a)ction, (p)ower, (b)ag, (i)nfo, (e)dit, (q)uit
 while(True):
-    choice = input("What would you like to do?\n (s)tatus, (c)heck, (a)ction, (p)ower, (b)ag, (i)nfo, (e)dit\n\n")
+    choice = input("What would you like to do?\n (s)tatus, (c)heck, (a)ction, (p)ower, (b)ag, (i)nfo, (e)dit, (q)uit\n\n")
     choice = choice.lower()
-    if choice == 's' or choice == 'S':
+    if choice == 's':
         print("\n")
         status()
-    elif choice == 'c' or choice == 'C':
+    elif choice == 'c':
         print("\n")
-        check()
+        print(check())
+    elif choice == 'q':
+        sys.exit(0)
     print("\n")
