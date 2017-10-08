f0853e853a8b6d629ae6474f01e214824e64eb74
ssono <blusentinel32@gmail.com>
2017-10-08 11:59:56 -0400

    everything works

--- ssonodnd.py
+++ ssonodnd.py
@@ -2,48 +2,6 @@ import json
 import random
 import sys
 
-atwill = {
-    "careful attack": {
-        "effect": "ranged or meelee attack",
-        "rattack": ("dex", "2", "AC", 1),
-        "mattack": ("str", "2", "AC", 1),
-        "ranged": ([8], "[0]"),
-        "melee": ([4], "[0]")
-    },
-    "nimble strike": {
-        "effect": "shift 1 before or after. ranged attack",
-        "attack": ("dex", "0", "AC", 1),
-        "ranged": ([8], "[(ability['dex'] - 10)//2]")
-    }
-}
-
-encounter = {
-    "evasive strike": {
-        "effect": "shift 1 + wis before or after. ranged or melee attack",
-        "rattack": ("dex", "0", "AC", 1),
-        "mattack": ("str", "0", "AC", 1),
-        "ranged": ([8, 8], "[(ability['dex'] - 10)//2]"),
-        "melee": ([4, 4], "[(ability['str'] - 10)//2]")
-    }
-}
-
-daily = {
-    "split tree": {
-        "effect": "attack 2 character within 3 spaces. Roll attack twice use higher",
-        "attack": ("dex", "0", "AC", 2),
-        "ranged": ([8, 8], "[(ability['dex'] - 10)//2]")
-    }
-}
-
-utility = {
-    "second chance": {
-        "effect": "DAILY-- When you are hit by an enemy, force them to roll again"
-    },
-    "crucial advice": {
-        "effect": "ENCOUNTER-- An ally gets a bonus to a skill check in a skill you are trained in equal to wis"
-    }
-}
-
 #ability{name: score}
 #info{name: val}
 #skills{name: [trained bonus, misc bonus]}
@@ -79,6 +37,7 @@ def atwillLoad():
         json.dump(atwill, f_obj)
 def encounterLoad():
     filename = 'encounter.json'
+    print("enc")
     with open(filename, 'w') as f_obj:
         json.dump(encounter, f_obj)
 def dailyLoad():
@@ -155,21 +114,38 @@ utility = utilityDump()
 def roll(dice, bonuses):
     result = 0
     for d in dice:
-        result += random.randint(1, d)
+        r = random.randint(1, d)
+#        print("die:\t" + str(r))
+        result += r
     for b in bonuses:
         result += b
+#        print("bonus:\t" + str(b))
     return result
 
 def d20():
     return random.randint(1, 20)
+
+def calcBonus(ability, raw):
+    result = raw
+    if raw in ["con", "str", "dex", "int", "wis", "cha"]:
+        result = (ability[raw]-10)//2
+    result = int(result)
+    return result
+
+def bonusList(ability, blist):
+    result = []
+    for item in blist:
+        result.append(calcBonus(ability, item))
+
+    return result
 #####################################################################################################################################################
-initiative = (info["level"]//2) + (ability["dex"] - 10)//2
+initiative = (info["level"]//2) + calcBonus(ability, 'dex')
 
 speed = 6
 
-passiveInsight = 10 + skills["ins"][1] + (info["level"]//2) + (ability["wis"] - 10)//2 + skills["ins"][0]
+passiveInsight = 10 + skills["ins"][1] + (info["level"]//2) + calcBonus(ability, 'wis') + skills["ins"][0]
 
-passivePerception = 10 + skills["per"][1] + info["level"]//2 + (ability["wis"] - 10)//2 + skills["per"][0]
+passivePerception = 10 + skills["per"][1] + info["level"]//2 + calcBonus(ability, 'wis') + skills["per"][0]
 
 maxHP = 12 + ability["con"] + 5*(info["level"] - 1)
 HP = maxHP
@@ -177,10 +153,10 @@ bloodied = False
 surgeVal = maxHP // 4
 surgePerDay = 6
 
-AC = 10 + info["level"]//2 + 2 + (ability["dex"] - 10)//2
-FORT = 10 + info["level"]//2 + 1 + (ability["str"] - 10)//2
-REF = 10 + info["level"]//2 + 1 + (ability["dex"] - 10)//2
-WILL = 10 + info["level"]//2 + (ability["wis"] - 10)//2
+AC = 10 + info["level"]//2 + 2 + calcBonus(ability, 'dex')
+FORT = 10 + info["level"]//2 + 1 + calcBonus(ability, 'str')
+REF = 10 + info["level"]//2 + 1 + calcBonus(ability, 'dex')
+WILL = 10 + info["level"]//2 + calcBonus(ability, 'wis')
 
 weight = 0
 for item in bag:
@@ -207,55 +183,55 @@ def check():
     result = "That is not a skill you possess..."
     if skill == "acr":
         roll = d20()
-        return("acr:\t" + str(roll + info["level"]//2 + (ability["dex"]-10)//2 + skills["acr"][0] + skills["acr"][1]) + "\troll:\t" + str(roll))
+        return("acr:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'dex') + skills["acr"][0] + skills["acr"][1]) + "\troll:\t" + str(roll))
     elif skill == "arc":
         roll = d20()
-        return("arc:\t" + str(roll + info["level"]//2 + (ability["int"]-10)//2 + skills["arc"][0] + skills["arc"][1]) + "\troll:\t" + str(roll))
+        return("arc:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'int') + skills["arc"][0] + skills["arc"][1]) + "\troll:\t" + str(roll))
     elif skill == "ath":
         roll = d20()
-        return("ath:\t" + str(roll + info["level"]//2 + (ability["str"]-10)//2 + skills["ath"][0] + skills["ath"][1]) + "\troll:\t" + str(roll))
+        return("ath:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'str') + skills["ath"][0] + skills["ath"][1]) + "\troll:\t" + str(roll))
     elif skill == "blu":
         roll = d20()
-        return("blu:\t" + str(roll + info["level"]//2 + (ability["cha"]-10)//2 + skills["blu"][0] + skills["blu"][1]) + "\troll:\t" + str(roll))
+        return("blu:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'cha') + skills["blu"][0] + skills["blu"][1]) + "\troll:\t" + str(roll))
     elif skill == "dip":
         roll = d20()
-        return("dip:\t" + str(roll + info["level"]//2 + (ability["cha"]-10)//2 + skills["dip"][0] + skills["dip"][1]) + "\troll:\t" + str(roll))
+        return("dip:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'cha') + skills["dip"][0] + skills["dip"][1]) + "\troll:\t" + str(roll))
     elif skill == "dun":
         roll = d20()
-        return("dun:\t" + str(roll + info["level"]//2 + (ability["wis"]-10)//2 + skills["dun"][0] + skills["dun"][1]) + "\troll:\t" + str(roll))
+        return("dun:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'wis') + skills["dun"][0] + skills["dun"][1]) + "\troll:\t" + str(roll))
     elif skill == "end":
         roll = d20()
-        return("end:\t" + str(roll + info["level"]//2 + (ability["con"]-10)//2 + skills["end"][0] + skills["end"][1]) + "\troll:\t" + str(roll))
+        return("end:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'con') + skills["end"][0] + skills["end"][1]) + "\troll:\t" + str(roll))
     elif skill == "hea":
         roll = d20()
-        return("hea:\t" + str(roll + info["level"]//2 + (ability["wis"]-10)//2 + skills["hea"][0] + skills["hea"][1]) + "\troll:\t" + str(roll))
+        return("hea:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'wis') + skills["hea"][0] + skills["hea"][1]) + "\troll:\t" + str(roll))
     elif skill == "his":
         roll = d20()
-        return("his:\t" + str(roll + info["level"]//2 + (ability["int"]-10)//2 + skills["his"][0] + skills["his"][1]) + "\troll:\t" + str(roll))
+        return("his:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'int') + skills["his"][0] + skills["his"][1]) + "\troll:\t" + str(roll))
     elif skill == "ins":
         roll = d20()
-        return("ins:\t" + str(roll + info["level"]//2 + (ability["wis"]-10)//2 + skills["ins"][0] + skills["ins"][1]) + "\troll:\t" + str(roll))
+        return("ins:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'wis') + skills["ins"][0] + skills["ins"][1]) + "\troll:\t" + str(roll))
     elif skill == "int":
         roll = d20()
-        return("int:\t" + str(roll + info["level"]//2 + (ability["cha"]-10)//2 + skills["int"][0] + skills["int"][1]) + "\troll:\t" + str(roll))
+        return("int:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'cha') + skills["int"][0] + skills["int"][1]) + "\troll:\t" + str(roll))
     elif skill == "nat":
         roll = d20()
-        return("nat:\t" + str(roll + info["level"]//2 + (ability["wis"]-10)//2 + skills["nat"][0] + skills["nat"][1]) + "\troll:\t" + str(roll))
+        return("nat:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'wis') + skills["nat"][0] + skills["nat"][1]) + "\troll:\t" + str(roll))
     elif skill == "per":
         roll = d20()
-        return("per:\t" + str(roll + info["level"]//2 + (ability["wis"]-10)//2 + skills["per"][0] + skills["per"][1]) + "\troll:\t" + str(roll))
+        return("per:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'wis') + skills["per"][0] + skills["per"][1]) + "\troll:\t" + str(roll))
     elif skill == "rel":
         roll = d20()
-        return("rel:\t" + str(roll + info["level"]//2 + (ability["int"]-10)//2 + skills["rel"][0] + skills["rel"][1]) + "\troll:\t" + str(roll))
+        return("rel:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'int') + skills["rel"][0] + skills["rel"][1]) + "\troll:\t" + str(roll))
     elif skill == "ste":
         roll = d20()
-        return("ste:\t" + str(roll + info["level"]//2 + (ability["dex"]-10)//2 + skills["ste"][0] + skills["ste"][1]) + "\troll:\t" + str(roll))
+        return("ste:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'dex') + skills["ste"][0] + skills["ste"][1]) + "\troll:\t" + str(roll))
     elif skill == "str":
         roll = d20()
-        return("str:\t" + str(roll + info["level"]//2 + (ability["cha"]-10)//2 + skills["str"][0] + skills["str"][1]) + "\troll:\t" + str(roll))
+        return("str:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'cha') + skills["str"][0] + skills["str"][1]) + "\troll:\t" + str(roll))
     elif skill == "thi":
         roll = d20()
-        return("thi:\t" + str(roll + info["level"]//2 + (ability["dex"]-10)//2 + skills["thi"][0] + skills["thi"][1]) + "\troll:\t" + str(roll))
+        return("thi:\t" + str(roll + info["level"]//2 + calcBonus(ability, 'dex') + skills["thi"][0] + skills["thi"][1]) + "\troll:\t" + str(roll))
     return result
 #####################################################################################################################################################################################################
 def health(HP, maxHP):
@@ -272,7 +248,7 @@ def health(HP, maxHP):
     print("You have " + str(HP) + " HP\n")
     return HP
 ######################################################################################################################################################################################################
-def feats():
+def showFeats():
     for item in feat:
         print("#" + item + ":\t" + feat[item])
 ################################################################################################################################################################################################
@@ -294,7 +270,11 @@ def powers(atwill, encounter, daily, utility):
         usePower(daily)
     elif powChoice == 'u':
         print("\n")
-        show(utility)
+        show_util(utility)
+
+def show_util(utility):
+    for item in utility:
+        print("#"+item+":\t"+utility[item])
 
 def show(choice):
         for item in choice:
@@ -309,13 +289,16 @@ def usePower(powChoice):
     print("\n")
     if pwr != '':
         pwr = pwr.lower()
-        pwr = powChoice[pwr]
+        try:
+            pwr = powChoice[pwr]
+        except KeyError:
+            return
     rolz = []
     atype = 'r'
     if len(pwr) == 3:
-        for rol in range(pwr["attack"][3]):
+        for rol in range(pwr["rattack"][3]):
             d = d20()
-            rolz.append((d, d + int(pwr["attack"][1]) + (ability[pwr["attack"][0]] - 10)//2))
+            rolz.append((d, d + pwr["rattack"][1] + (calcBonus(ability, pwr["rattack"][0]) + info["level"]//2), pwr["rattack"][2]))
     elif len(pwr) == 5:
         rmChoice = input("Is this (r)anged or (m)elee\n")
         atype = rmChoice
@@ -323,23 +306,25 @@ def usePower(powChoice):
         if rmChoice == 'r':
             for rol in range(pwr["rattack"][3]):
                 d = d20()
-                rolz.append((d, d + int(pwr["rattack"][1]) + (ability[pwr["rattack"][0]] - 10)//2))
+                rolz.append((d, d + pwr["rattack"][1] + (calcBonus(ability, pwr["rattack"][0]) + info["level"]//2), pwr["rattack"][2]))
         elif rmChoice == 'm':
             for rol in range(pwr["mattack"][3]):
                 d = d20()
-                rolz.append((d, d + int(pwr["mattack"][1]) + (ability[pwr["mattack"][0]] - 10)//2))
+                rolz.append((d, d + pwr["mattack"][1] + (calcBonus(ability, pwr["mattack"][0]) + info["level"]//2), pwr["mattack"][2]))
     for num in rolz:
         print("base roll:\t" + str(num[0]))
-        print("total roll:\t" + str(num[1]))
+        print("total roll:\t" + str(num[1]) + "\tvs\t" + str(num[2]))
         print("\n")
     hit = input("did you hit?\n (y)es or (n)o\n")
     print("\n")
     dmg = 0
     if hit == 'y':
         if atype == 'r':
-            dmg = roll(pwr["ranged"][0], eval(pwr["ranged"][1]))
+            rblist = bonusList(ability, pwr["ranged"][1])
+            dmg = roll(pwr["ranged"][0], rblist)
         elif atype == 'm':
-            dmg = roll(pwr["melee"][0], eval(pwr["melee"][1]))
+            mblist = bonusList(ability, pwr["melee"][1])
+            dmg = roll(pwr["melee"][0], mblist)
     print("damage:\t" + str(dmg))
 ##################################################################################################################################################################################################
 def showBag(bag):
@@ -381,16 +366,152 @@ def showBag(bag):
         try:
             del bag[deletion]
         except KeyError:
-            pass
+            return
     return bag
 
 #################################################################################################################################################################################################
-def showInfo(info):
+def showInfo(info, ability, skills):
     for item in info:
         print(item + ":\t" + str(info[item]))
+    print("\n")
+    for item in ability:
+        print(item + ":\t" + str(ability[item]))
+    print("\n")
+    for item in skills:
+        print(item + ":\t trained: " + str(skills[item][0]) + "\t misc: " + str(skills[item][1]))
 #################################################################################################################################################################################################
-def edit_stuff(ability):
-    print("Your stuff is totes editted")
+def edit_stuff(ability, skills, info, feat, atwill, encounter, daily, utility):
+    choice = input("What would you like to edit?\n (a)bility, (s)kills, (i)nfo, (f)eats, (at)will, (e)ncounter, (d)aily, (u)tility\n")
+    print("\n")
+    if choice == 'a':
+        edit_ability(ability)
+    elif choice == 's':
+        edit_skills(skills)
+    elif choice == 'i':
+        edit_info(info)
+    elif choice == 'f':
+        edit_feats(feat)
+    elif choice == 'at':
+        edit_power(atwill, 'a')
+    elif choice == 'e':
+        edit_power(encounter, 'e')
+    elif choice == 'd':
+        edit_power(daily, 'd')
+    elif choice == 'u':
+        edit_utility()
+
+def edit_ability(ability):
+    for item in ability:
+        print(item + ":\t" + str(ability[item]))
+    abilChoice = input("What ability are you changing?\n")
+    print("\n")
+    nval = input("What is the new desired value?\n")
+    print("\n")
+    if nval != '':
+        try:
+            ability[abilChoice] = int(nval)
+        except KeyError:
+            return
+    abiLoad()
+
+def edit_skills(skills):
+    for item in skills:
+        print(item + ":\t trained: " + str(skills[item][0]) + "\t misc: " + str(skills[item][1]))
+    skilChoice = input("What skill would you like to change?\n")
+    print("\n")
+    nval = input("What is the new desired value?\n")
+    print("\n")
+    bonus_type = input("Is this a (t)raining or (m)isc bonus?\n")
+    print("\n")
+    if bonus_type == 't':
+        bonus_type = 0
+    elif bonus_type == 'm':
+        bonus_type = 1
+    else:
+        bonus_type = 2
+    if bonus_type == 0 or bonus_type == 1:
+        if nval != '':
+            try:
+                skills[skilChoice][bonus_type] = int(nval)
+            except KeyError:
+                return
+    skiLoad()
+
+def edit_info(info):
+    for item in info:
+        print(item + ":\t" + str(info[item]))
+    infoChoice = input("What info would you like to change?\n")
+    infoChoice = infoChoice.lower()
+    print("\n")
+    nval = input("what is the new desired value?\n")
+    print("\n")
+    if nval != '':
+        try:
+            if infoChoice == 'xp' or infoChoice == 'level':
+                info[infoChoice] = int(nval)
+            else:
+                info[infoChoice] = nval
+        except KeyError:
+            return
+    infoLoad()
+
+def edit_feats(feat):
+    for item in feat:
+        print("#"+item+":\t"+feat[item])
+    print("\n")
+    name = input("What is the name of your new feat \n")
+    print("\n")
+    effect = input("What is the effect of the new feat\n")
+    print("\n")
+    if name != '':
+        feat[name] = effect
+    featLoad()
+
+def edit_utility():
+    for item in utility:
+        print("#"+item+":\t"+utility[item])
+    print("\n")
+    name = input("What is the name of your new utility \n")
+    print("\n")
+    effect = input("What is the effect of the new utility\n")
+    print("\n")
+    if name != '':
+        utility[name] = effect
+    utilityLoad()
+
+def edit_power(powObj, typ):
+    name = input("What is the name of your power?\n")
+    print("\n")
+    effect = input("What is the effect of your power?\n")
+    print("\n")
+    rattack = input("What is the effect of your ranged attack? ['3-letter abilty', bonus, 'defense', num-of-rolls]\n")
+    print("\n")
+    ranged = input("What is the effect of your ranged damage? [[#, #, #], ['3-letter ability', #, #]]\n")
+    print("\n")
+    mattack = input("What is the effect of your melee attack? ['3-letter abilty', bonus, 'defense', num-of-rolls]\n")
+    print("\n")
+    melee = input("What is the effect of your melee damage? [[#, #, #], ['3-letter abilty', #, #]]\n")
+    print("\n")
+    if name != '' and effect != '' and ranged != '':
+        rattack = eval(rattack)
+        ranged = eval(ranged)
+        if melee != '' and mattack != '':
+            mattack = eval(mattack)
+            melee = eval(melee)
+    powObj[name] = {"effect": effect, "rattack": rattack, "ranged": ranged, "mattack": mattack, "melee": melee}
+    if typ == 'a':
+        atwill = powObj
+        atwillLoad()
+    elif typ == 'e':
+        enconuter = powObj
+        encounterLoad()
+    elif typ == 'd':
+        daily = powObj
+        dailyLoad()
+
+
+
+
 #################################################################################################################################################################################################
 #loop that asks what you want to do. (s)tatus, (c)heck, (h)ealth, (f)eats, (p)ower, (b)ag, (d)ice (i)nfo, (e)dit, (q)uit
 while(True):
@@ -408,7 +529,7 @@ while(True):
         HP = health(HP, maxHP)
     elif choice == 'f':
         print("\n")
-        feats()
+        showFeats()
     elif choice == 'p':
         print("\n")
         powers(atwill, encounter, daily, utility)
@@ -418,10 +539,10 @@ while(True):
         bagLoad()
     elif choice == 'i':
         print("\n")
-        showInfo(info)
+        showInfo(info, ability, skills)
     elif choice == 'e':
         print("\n")
-        edit_stuff(ability)
+        edit_stuff(ability, skills, info, feat, atwill, encounter, daily, utility)
     elif choice == 'q':
         sys.exit(0)
     else:
